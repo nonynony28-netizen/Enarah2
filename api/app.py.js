a75355f044124da5d2ba5c97866cc1c2f6de87f6
nonynony28-netizen <nonynony28@gmail.com>
2026-05-13 02:23:19 +0200

    Update app.py.js

--- api/app.py.js
+++ api/app.py.js
@@ -1,1 +1,27 @@
+# file name: app.py
 
+import os
+from groq import Groq
+
+# تأكد أنك ضفت المتغير في النظام:
+# Windows (PowerShell):
+# setx GROQ_API_KEY "your_api_key_here"
+#
+# Linux / Mac:
+# export GROQ_API_KEY="your_api_key_here"
+
+client = Groq(
+    api_key=os.environ.get("GROQ_API_KEY")
+)
+
+chat_completion = client.chat.completions.create(
+    messages=[
+        {
+            "role": "user",
+            "content": "Hello, how are you?"
+        }
+    ],
+    model="llama-3.3-70b-versatile"
+)
+
+print(chat_completion.choices[0].message.content)
