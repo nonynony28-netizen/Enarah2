4006bfb418121042d1abcfeb66b63d59383a8655
nonynony28-netizen <nonynony28-netizen@users.noreply.github.com>
2026-07-22 16:34:06 +0200

    fix: switch from HashRouter to BrowserRouter to fix direct routing page load on Vercel

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,6 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import { HashRouter } from 'react-router-dom'
+import { BrowserRouter } from 'react-router-dom'
 import { LanguageProvider } from './hooks/useLanguage.tsx'
 import { CartProvider } from './hooks/useCart.tsx'
 import './index.css'
@@ -8,12 +8,12 @@ import App from './App.tsx'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <HashRouter>
+    <BrowserRouter>
       <LanguageProvider>
         <CartProvider>
           <App />
         </CartProvider>
       </LanguageProvider>
-    </HashRouter>
+    </BrowserRouter>
   </StrictMode>,
 )
